8af9c5c90792df9e4fabcbd5b3acd55f8cd48edb
Brandon Quintero <developerbvth@gmail.com>
2024-02-20 11:41:36 -0500

    Implementando Sass

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import RoutePages from "./pages";
 import { Store } from "./context/useProvider";
-import "./assets/style.css";
+import "./assets/main.scss";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Store>
     <BrowserRouter>
