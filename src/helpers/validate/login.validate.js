033ebf7958dd2e5e918bde767a0c7dd3d0997cfd
xChichoTop <johanparra2319@gmail.com>
2024-02-20 14:21:25 -0500

    Login styles

--- src/helpers/validate/login.validate.js
+++ src/helpers/validate/login.validate.js
@@ -4,8 +4,8 @@ export const SchemaLoginValidate = yup.object().shape({
   correo: yup.string().email("El email no es valido"),
   clave: yup
     .string()
-    .min(6, "la contraseña tiene que ser mas de 6 caracteres")
-    .max(15, "la contraseña tiene que ser menos de 15 caracteres")
-    .matches(/[0-9]/, "Debe contener al menos un número")
-    .matches(/[a-zA-Z]/, "Debe contener al menos un carácter de tipo letra"),
+    .min(6, "La contraseña debe tener mínimo 6 caracteres")
+    .max(15, "La contraseña debe tener máximo 15 caracteres")
+    .matches(/[0-9]/, "La contraseña debe contener al menos un número")
+    .matches(/[a-zA-Z]/, "La contraseña debe contener al menos una letra"),
 });
--- src/pages/login/login.routes.jsx
+++ src/pages/login/login.routes.jsx
@@ -6,6 +6,8 @@ import { DATA_URL_LOGIN } from "../../assets/DATA_URL";
 import { isAxiosError } from "axios";
 import { ValidationError } from "yup";
 import { InputPassword } from "./others/password.component";
+
+
 export function Login() {
   const [err, setError] = useState(null);
 
@@ -20,14 +22,24 @@ export function Login() {
       const res = await ApiPost(DATA_URL_LOGIN, envio);
 
       if (isAxiosError(res)) {
-        console.log("instance error ");
+        console.log("instance error");
         setError(res.response.data.message);
+
+        // Configurar el temporizador para limpiar el error después de 4 segundos
+        setTimeout(() => {
+          setError(null);
+        }, 4000);
       } else {
         window.location.href = "/";
       }
     } catch (error) {
       if (error instanceof ValidationError) {
         setError(error.message);
+
+        // Configurar el temporizador para limpiar el error después de 4 segundos
+        setTimeout(() => {
+          setError(null);
+        }, 4000);
       } else {
         console.log(error);
         setError("Error desconocido en login routes");
@@ -37,62 +49,80 @@ export function Login() {
 
   return (
     <>
-      <form
-        onSubmit={handleSubmit}
-        className="login-container bg-gray d-flex justify-content-center align-items-center vh-100  "
-      >
-        <div
-          className="login-content bg-white p-5 rounded-5 text-secondary"
-          style={{ width: "25rem" }}
-        >
-          <div className="d-flex justify-content-center"></div>
-          <div className="text-center fs-1 fw-bold">Iniciar Sesión</div>
-          <div className="input-group mt-4">
-            <div className="input-group-text bg-gray">
-              <i className="far fa-envelope"></i>
+
+    <div className="divBackground">
+
+      <div className="divMainLogin d-flex">
+
+
+        <form onSubmit={handleSubmit} className=" d-flex justify-content-center align-items-center">
+
+          <div className="divLeft login-content bg-white p-5 rounded-5 text-secondary" style={{ width: "25rem" }}>
+            <div className="d-flex justify-content-center"></div>
+            <div className="text-center fs-1 fw-bold">Iniciar Sesión</div>
+            <div className="input-group mt-4">
+              <div className="input-group-text bg-gray">
+                <i className="far fa-envelope"></i>
+              </div>
+              <input
+                className="form-control bg-light"
+                type="text"
+                placeholder="Usuario"
+                name="email"
+              />
             </div>
-            <input
-              className="form-control bg-light"
-              type="text"
-              placeholder="Usuario"
-              name="email"
-            />
-          </div>
-          <div className="input-group mt-1">
-            <div className="input-group-text bg-gray">
-              <i className="fas fa-lock"></i>
+            <div className="input-group mt-1">
+              <div className="input-group-text bg-gray">
+                <i className="fas fa-lock"></i>
+              </div>
+              <InputPassword />
             </div>
-            <InputPassword />
-          </div>
-          <div className="d-flex justify-content-around mt-1">
-            <div className="pt-1">
+            <div className="d-flex justify-content-around mt-1">
+              <div className="pt-1">
+                <NavLink
+                  to="/authenticate/recovery"
+                  className="text-decoration-none text-info fw-semibold fst-italic"
+                >
+                  ¿Olvidaste la contraseña?
+                </NavLink>
+              </div>
+            </div>
+            <button className="btn btn-secondary  text-white w-100 mt-2 fw-semibold shadow-sm">
+              Ingresar
+            </button>
+            <div className="d-flex gap-1 justify-content-center mt-1">
+              <div>¿No tienes una cuenta?</div>
               <NavLink
-                to="/authenticate/recovery"
-                className="text-decoration-none text-info fw-semibold fst-italic"
+                to="/authenticate/register"
+                className="text-decoration-none text-info fw-semibold"
               >
-                ¿Olvidaste la contraseña?
+                Registrarse
               </NavLink>
             </div>
+            {err && (
+              <div className="alert alert-danger" role="alert">
+                {err}
+              </div>
+            )}
           </div>
-          <button className="btn btn-secondary  text-white w-100 mt-2 fw-semibold shadow-sm">
-            Ingresar
-          </button>
-          <div className="d-flex gap-1 justify-content-center mt-1">
-            <div>¿No tienes una cuenta?</div>
-            <NavLink
-              to="/authenticate/register"
-              className="text-decoration-none text-info fw-semibold"
-            >
-              Registrarse
-            </NavLink>
-          </div>
-          {err && (
-            <div className="alert alert-danger" role="alert">
-              {err}
-            </div>
-          )}
+
+
+
+        </form>
+
+        
+        <div>
+          <img src="https://p5.itc.cn/q_70/images03/20211101/50e200294c84426ea1d6d79b47cbcf56.gif" alt="" width="400px"/>
         </div>
-      </form>
+
+      </div>
+
+  
+     
+
+      </div>
+
+
     </>
   );
 }
