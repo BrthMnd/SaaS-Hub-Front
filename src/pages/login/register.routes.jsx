84a871bde6653c7771c1e06a324280bb279b00cf
xChichoTop <johanparra2319@gmail.com>
2024-02-26 10:39:53 -0500

    Front in register and others details

--- src/pages/login/register.routes.jsx
+++ src/pages/login/register.routes.jsx
@@ -4,6 +4,8 @@ import { NavLink, useNavigate } from "react-router-dom";
 import { isAxiosError } from "axios";
 import { SchemaLoginValidate } from "../../helpers/validate/login.validate";
 import { ApiPost } from "../../hooks/useApi";
+import loginGif from "../../assets/images/LoginGif.gif"
+
 
 export function Register() {
   const navigate = useNavigate();
@@ -11,8 +13,11 @@ export function Register() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    if (e.target.password.value != e.target.vpassword.value) {
+    if (e.target.password.value !== e.target.vpassword.value) {
       setError("Las contraseñas no son iguales");
+      setTimeout(() => {
+        setError(null); // Eliminar el error después de 4 segundos
+      }, 4000);
       return;
     }
     const envio = {
@@ -29,6 +34,7 @@ export function Register() {
       if (isAxiosError(res)) {
         console.log("instance error ");
         setError(res.response.data.message);
+        setTimeout(() => setError(null), 4000); // Ocultar la alerta después de 4 segundos
       } else {
         window.location.reload();
       }
@@ -45,85 +51,90 @@ export function Register() {
   return (
     <>
       <div className="divBackground">
+        <div className="divMainLogin d-flex">
 
-        <form
-          className="login-container bg-gray d-flex justify-content-center align-items-center"
-          onSubmit={handleSubmit}
-        >
-          <div
-            className="login-content bg-white p-5 rounded-5 text-secondary"
-            style={{ width: "25rem" }}
+          <form
+            className="login-container d-flex justify-content-center align-items-center"
+            onSubmit={handleSubmit}
           >
-            <div className="d-flex justify-content-center"></div>
-            <div className="text-center fs-1 fw-bold">Registro</div>
+            <div className="divLeft login-content bg-white p-5 rounded-5 text-secondary" style={{ width: "25rem" }}>
 
-            <div className="input-group mt-3">
-              <div className="input-group-text bg-gray">
-                <i className="far fa-envelope"></i>
+              <div className="d-flex justify-content-center"></div>
+              <div className="text-center fs-1 fw-bold">Registro</div>
+
+              <div className="input-group mt-3">
+                <div className="input-group-text bg-dark">
+                  <i className="far fa-envelope"></i>
+                </div>
+                <input className="form-control bg-light" type="text" placeholder="Nombre" name="nombre" required />
               </div>
-              <input className="form-control bg-light" type="text" placeholder="Nombre" name="nombre" required/>
+
+              <div className="input-group mt-3">
+                <div className="input-group-text bg-dark">
+                  <i className="far fa-envelope"></i>
+                </div>
+                <input className="form-control bg-light" type="text" placeholder="Correo" name="email" required />
+              </div>
+
+
+              <div className="input-group mt-3">
+                <div className="input-group-text bg-dark">
+                  <i className="fas fa-lock"></i>
+                </div>
+                <input className="form-control bg-light" type="password" placeholder="Contraseña" name="password" required />
+              </div>
+
+              <div className="input-group mt-3">
+                <div className="input-group-text bg-dark">
+                  <i className="fas fa-lock"></i>
+                </div>
+                <input className="form-control bg-light" type="password" placeholder="Verificar Contraseña" name="vpassword" title="Verifica la contraseña" required />
+              </div>
+
+              <div className="input-group mt-3">
+
+                <div className="input-group-text bg-dark">
+                  <i className="fas fa-venus-mars"></i>
+                </div>
+
+                <select className="form-control bg-light" id="genero" name="genero" required>
+                  <option value="" disabled selected hidden>
+                    Género
+                  </option>
+                  <option value="masculino">Masculino</option>
+                  <option value="femenino">Femenino</option>
+                  <option value="otro">Otro</option>
+                </select>
+
+              </div>
+
+              <button className="btn btn-dark  text-white w-100 mt-2 fw-semibold shadow-sm">
+                Registrar
+              </button>
+              <div className="d-flex gap-1 justify-content-center mt-3">
+                <div>Ya tienes una cuenta? </div>
+                <NavLink
+                  to="/authenticate/login"
+                  className="text-decoration-none text-info fw-semibold"
+                >
+                  Ingresa
+                </NavLink>
+              </div>
+              {err && (
+                <div className="alert alert-danger" role="alert">
+                  {err}
+                </div>
+              )}
             </div>
 
-            <div className="input-group mt-3">
-              <div className="input-group-text bg-gray">
-                <i className="far fa-envelope"></i>
-              </div>
-              <input className="form-control bg-light" type="text" placeholder="Correo" name="email" required/>
-            </div>
+          </form>
 
-
-            <div className="input-group mt-3">
-              <div className="input-group-text bg-gray">
-                <i className="fas fa-lock"></i>
-              </div>
-              <input className="form-control bg-light" type="password" placeholder="Contraseña" name="password" required/>
-            </div>
-
-            <div className="input-group mt-3">
-              <div className="input-group-text bg-gray">
-                <i className="fas fa-lock"></i>
-              </div>
-              <input className="form-control bg-light" type="password" placeholder="Verificar Contraseña" name="vpassword"  title="Verifica la contraseña" required/>
-            </div>
-
-            <div className="input-group mt-3">
-              <div className="input-group-text bg-gray">
-                <i className="fas fa-venus-mars"></i>
-              </div>
-              <select
-                className="form-control bg-light"
-                id="genero"
-                name="genero"
-                required
-              >
-                <option value="" disabled selected hidden>
-                  Género
-                </option>
-                <option value="masculino">Masculino</option>
-                <option value="femenino">Femenino</option>
-                <option value="otro">Otro</option>
-              </select>
-            </div>
-            <button className="btn btn-secondary  text-white w-100 mt-2 fw-semibold shadow-sm">
-              Registrar
-            </button>
-            <div className="d-flex gap-1 justify-content-center mt-3">
-              <div>Ya tienes una cuenta? </div>
-              <NavLink
-                to="/authenticate/login"
-                className="text-decoration-none text-info fw-semibold"
-              >
-                Ingresa
-              </NavLink>
-            </div>
-            {err && (
-              <div className="alert alert-danger" role="alert">
-                {err}
-              </div>
-            )}
+          <div className="gifLogin">
+            <img src="" alt="" />
+            <img src={loginGif} alt="" width="450px" />
           </div>
 
-        </form>
+        </div>
 
       </div>
     </>
